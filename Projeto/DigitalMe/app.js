70c71644ef5bc6fed08626665f66b8fa4f0020b9
ricardo68468 <a68468@alunos.uminho.pt>
2018-01-18 17:54:49 +0000

    a

--- Projeto/DigitalMe/app.js
+++ Projeto/DigitalMe/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 
 
 var app = express();
@@ -15,21 +17,42 @@ mongoose.Promise = global.Promise
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
+app.use(favicon());
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+// TODO - Why Do we need this key ?
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+ // Using the flash middleware provided by connect-flash to store messages in session
+ // and displaying in templates
  
-var routes = require('./routes/index');
+var flash = require('connect-flash');
+app.use(flash());
+
+// Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
+var routes = require('./routes/index')(passport);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- Projeto/DigitalMe/models/schema.js
+++ Projeto/DigitalMe/models/schema.js
@@ -4,15 +4,11 @@ var Schema = mongoose.Schema
 var UserSchema = new Schema(
     {
         email: {type: String, required: true},
-        nome: {type: String, required: true},
+        name: {type: String, required: true},
         password: {type: String, required: true},
-        genero: {type:String, required: true},
-        data_nasc: {type:String}
-<<<<<<< HEAD
+        //genero: {type:String},
+        //data_nasc: {type:String}
     }, {collection: 'testes'}
-=======
-    },{collection:"testes"}
->>>>>>> 637a185caefd88757f12abe4ef037268d818e96a
 )
 
 
